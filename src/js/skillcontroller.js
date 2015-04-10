2c4a26ede8f63ef5660536a9a5910b896af80d1f
Ives van Hoorne <Ives.v.h@gmail.com>
2015-04-10 12:43:07 +0200

    Disabled Uglify for now

--- src/js/skillcontroller.js
+++ src/js/skillcontroller.js
@@ -1,6 +1,8 @@
-app.controller("SkillController", function() {
-    this.title = "Coding Skills";
-    this.setContent = function(skill) {
-        this.title = skill;
-    };
-});
+app.controller("SkillController", [
+    function() {
+        this.title = "Coding Skills";
+        this.setContent = function(skill) {
+            this.title = skill;
+        };
+    }
+]);
