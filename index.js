aab7c20788c5ff3f6ad910dbacb61555a30c666a
Ives van Hoorne <Ives.v.h@gmail.com>
2015-04-14 23:23:16 +0200

    Updated server file Removed margin from build toold

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ var favicon = require('serve-favicon');
 
 var app = module.exports = express();
 
-
-if (app.get('env') == 'development') {
-    var browserSync = require('browser-sync');
-    var bs = browserSync({
-        logSnippet: false,
-        // Serve files from the app directory
-        server: {
-            baseDir: "public",
-            directory: true
-        },
-        files: "public/**"
-    });
-    app.use(require('connect-browser-sync')(bs));
-    console.log("Browsersync has been activated");
-}
-
 /**
  * Configuration
  */
@@ -27,7 +11,7 @@ if (app.get('env') == 'development') {
 // all environments
 app.set('port', process.env.PORT || 3000);
 //app.use(favicon(path.join(__dirname, "public", "res", "favicon.ico")));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 
 var server = app.listen(app.get('port'), function() {
