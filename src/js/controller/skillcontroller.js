f53d3309ce6b3b9003a8d1db158261e2fc24030d
Ives van Hoorne <Ives.v.h@gmail.com>
2015-05-02 14:15:08 +0200

    Removed redundant skillcontroller

--- src/js/controller/skillcontroller.js
+++
@@ -1,8 +0,0 @@
-app.controller("SkillController", [
-    function() {
-        this.title = "Coding Skills";
-        this.setContent = function(skill) {
-            this.title = skill;
-        };
-    }
-]);
