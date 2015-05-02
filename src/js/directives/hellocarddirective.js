32687ad85914608148dccdbcbca7cef854898b4b
Ives van Hoorne <Ives.v.h@gmail.com>
2015-05-02 13:42:17 +0200

    Feedback from Facebook

--- src/js/directives/hellocarddirective.js
+++ src/js/directives/hellocarddirective.js
@@ -1,13 +1,19 @@
-app.directive("ngHello", function() {
-    return {
-        restrict: 'AE',
-        templateUrl: '/js/directives/hellocarddirective.html',
-        link: function(scope, element, attr, ctrl) {
-            $(element).find('.fa').click(function() {
-                $(element).find('.fronttextcard').toggleClass('active');
-                $(element).find('.backtextcard').toggleClass('active');
-                $(element).toggleClass('backactive');
-            });
-        }
-    };
-});
+app.directive("ngHello", ["ColorService",
+    function(ColorService) {
+        return {
+            restrict: 'AE',
+            templateUrl: '/js/directives/hellocarddirective.html',
+            link: function(scope, element, attr, ctrl) {
+                $(element).find('.fa').click(function() {
+                    $(element).find('.fronttextcard').toggleClass('active');
+                    $(element).find('.backtextcard').toggleClass('active');
+                    $(element).toggleClass('backactive');
+                });
+
+                $(element).find('.title').css({
+                    color: ColorService.colors.darkcolor
+                });
+            }
+        };
+    }
+]);
